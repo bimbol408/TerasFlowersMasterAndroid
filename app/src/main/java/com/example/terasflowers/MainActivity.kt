f748852a43d2e86b38b185e25b35f5ecbb71278c
bimbol408 <bimoario408@gmail.com>
2021-03-23 11:48:02 +0700

    First init

---
+++ app/src/main/java/com/example/terasflowers/MainActivity.kt
@@ -0,0 +1,81 @@
+package com.example.terasflowers
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import com.example.terasflowers.activity.LoginActivity
+import com.example.terasflowers.fragment.AkunFragment
+import com.example.terasflowers.fragment.HomeFragment
+import com.example.terasflowers.fragment.KeranjangFragment
+import com.example.terasflowers.helper.SharedPref
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+
+    private val fragmentHome: Fragment = HomeFragment()
+    private val fragmentKeranjang: Fragment = KeranjangFragment()
+    private var fragmentAkun: Fragment = AkunFragment()
+    private val fm: FragmentManager = supportFragmentManager
+    private var active: Fragment = fragmentHome
+
+    private lateinit var menu: Menu
+    private lateinit var menuItem: MenuItem
+    private lateinit var bottomNavigationView: BottomNavigationView
+
+    private var statusLogin = false
+
+    private lateinit var s:SharedPref
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        s = SharedPref(this)
+
+        setUpBottomNav()
+    }
+
+    fun setUpBottomNav(){
+        fm.beginTransaction().add(R.id.container, fragmentHome).show(fragmentHome).commit()
+        fm.beginTransaction().add(R.id.container, fragmentKeranjang).hide(fragmentKeranjang).commit()
+        fm.beginTransaction().add(R.id.container, fragmentAkun).hide(fragmentAkun).commit()
+
+        bottomNavigationView = findViewById(R.id.nav_view)
+        menu = bottomNavigationView.menu
+        menuItem = menu.getItem( 0)
+        menuItem.isChecked = true
+
+        bottomNavigationView.setOnNavigationItemSelectedListener { item ->
+
+            when(item.itemId) {
+                R.id.navigation_home ->{
+                    callFargment(0, fragmentHome)
+                }
+                R.id.navigation_keranjang ->{
+                    callFargment(1, fragmentKeranjang)
+                }
+                R.id.navigation_akun ->{
+                    if (s.getStatusLogin()) {
+                        callFargment(2, fragmentAkun)
+                    } else {
+                        startActivity(Intent(this, LoginActivity::class.java))
+                    }
+
+                }
+            }
+
+            false
+        }
+    }
+
+    fun callFargment(int: Int, fragment: Fragment) {
+        menuItem = menu.getItem(int)
+        menuItem.isChecked = true
+        fm.beginTransaction().hide(active).show(fragment).commit()
+        active = fragment
+    }
+}
